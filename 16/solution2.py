e8f399c428f72ca22b0006aca1746a21a5418e73
Wojciech Pędrak <woj123@gmail.com>
2019-12-17 00:16:16 +0100

    try over 16_2 (some random lines with fft)

--- 16/solution2.py
+++ 16/solution2.py
@@ -1,36 +1,33 @@
 import numpy as np
-import itertools
-
-
-def get_pattern(length_of_single, length_of_pattern):
-    base = ([0] * length_of_single) + ([1] * length_of_single) + ([0] * length_of_single) + ([-1] * length_of_single)
-    it = itertools.cycle(base)
-    next(it)
-    return [next(it) for _ in range(length_of_pattern)]
 
 
 def fft(numbers):
-    res = []
-    arr_numbers = np.array(numbers).reshape(-1, 1)
-    for idx in range(len(numbers)):
-        pattern = np.array(get_pattern(idx+1, len(numbers))).reshape(1, -1)
-        new_number = (np.abs(pattern.dot(arr_numbers)) % 10)[0, 0]
-        res.append(new_number)
-
-    return res
+    arr_numbers = np.array(numbers)
+    ones = 
+    compl = np.fft.fft(arr_numbers)
+    print('---------------------------------------------')
+    print(arr_numbers)
+    print(compl)
+    print(np.absolute(compl))
+    print('---------------------------------------------')
+    mask = (np.sign(compl.real) + np.sign(compl.imag)) / 2
+    results = np.absolute(compl) * mask
+    return list(results)
 
 
 def solve(message, iterations):
-    offset = int(message[:7])
+    # offset = int(message[:7])
     # print(offset)
-    numbers = [int(x) for x in message] * 10000
+    numbers = [int(x) for x in message]  # * 10000
     for i in range(iterations):
         print("Iteration", i)
         numbers = fft(numbers)
+        print(numbers)
 
-    return [str(x) for x in numbers][offset:offset + 8]
+    return numbers
+    # return [str(x) for x in numbers][offset:offset + 8]
 
 
-message = "03036732577212944063491565474664"
-res = solve(message, 100)
+message = "12345678"
+res = solve(message, 4)
 print("".join(res))
