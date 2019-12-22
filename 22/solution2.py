0ec369e4df7df08298acb1b6bb5185267971a0f8
Wojciech Pędrak <woj123@gmail.com>
2019-12-23 00:18:58 +0100

    22_2

--- 22/solution2.py
+++ 22/solution2.py
@@ -1,4 +1,5 @@
 import math
+from stolen_code import modinv
 
 DECK_SIZE = 119315717514047
 
@@ -8,11 +9,11 @@ def get_line_params(line):
     first = splitted[0]
     second = splitted[1]
     if first == 'cut':
-        return 1, - int(second)
+        return (1, int(second))
     elif second == 'into':
-        return -1, -1
+        return (-1, -1)
     elif second == 'with':
-        return int(splitted[-1]), 0
+        return (modinv(int(splitted[-1]), DECK_SIZE), 0)
 
 
 def input_as_operator(nrepeat=1):
@@ -22,7 +23,7 @@ def input_as_operator(nrepeat=1):
     mul = 1
     add = 0
 
-    for line in lines:
+    for line in reversed(lines):
         a, b = get_line_params(line)
         mul = mul * a
         add = add * a + b
